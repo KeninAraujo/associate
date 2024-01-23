c3789cdec2b4e98421112368a9832ec081b01176
Kenin <kenindr02@gmail.com>
2024-01-22 22:33:13 -0300

    add headless in bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ async function getShortLinks() {
     try {
         const browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            headless: true,
           });
           
         const page = await browser.newPage();
